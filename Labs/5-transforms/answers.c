300c9a62d108af56f2feda8f2f93abb1e6b133dc
Justin Weigle <jw6858@cs.ship.edu>
2019-10-06 00:20:39 -0400

    worked through questions 1-3

---
+++ Labs/5-transforms/answers.c
@@ -0,0 +1,13 @@
+
+
+Q1:
+    Translate appears first
+Q2:
+    Translate is applied first
+Q3:
+    No, the triangle appears in the center with the translate first and at the
+    side with rotate first. This is due to the face that when translated first,
+    the triangle starts the rotation out to the right where it was translated
+    to. But when it is rotated first, it just translates upwards.
+Q4:
+
