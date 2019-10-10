f4611e6d522260154e3c318fceb74fdfc61e7598
Justin Weigle <jw6858@cs.ship.edu>
2019-10-09 21:39:54 -0400

    up to Q11 done

--- Labs/6-projection/answers.c
+++ Labs/6-projection/answers.c
@@ -5,7 +5,27 @@ Q2:
     any distance becomes less and less impactful on the perceived vertical fov.
 
 Q4:
-    The values I used were -5.0, 5.0, -5.0, 5.0
+    The values I used were -6.0, 6.0, -6.0, 6.0
  
 Q5:
-    This image appears much more realistic than the images from Q2.
+    This image appears much more realistic than the images from Q2. It is not
+    nearly as distorted and the wire sphere on top shows more detail.
+
+Q7:
+    The only thing visible is the center of the pillar. The second one, where
+    the camera is 100 units from the origin, makes the pillar appear closer
+    even though the near clip plane is still the same distance from the pillar.
+    The values used were camera: 50 ncp: 20, camera: 100 ncp: 70. Using these,
+    50 - 30 = 20 units from pillar center and 100 - 80 = 20 from center.
+
+Q9:
+    fov: 8.55 aspect: 1.0
+
+Q10:
+    The image doesn't change at all.
+
+Q11:
+    Parallel viewing does not have a viewing angle that changes how much of
+    the image you see from different distances and it does not distort the
+    image as you get closer to it. Perspective views have a viewing angle that
+    can potentially distort the image as you get closer and closer to it.
