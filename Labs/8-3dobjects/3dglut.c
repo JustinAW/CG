9a20cc97c87e01c46e98c9654a6650c2aa26b970
Justin Weigle <jw6858@cs.ship.edu>
2019-10-22 16:52:32 -0400

    up to q7 done

--- Labs/8-3dobjects/3dglut.c
+++ Labs/8-3dobjects/3dglut.c
@@ -9,7 +9,7 @@
 /* Skeleton for displaying glut 3D primitives
 */
 
-#include <GL\glut.h>
+#include <GL/glut.h>
 #include <stdio.h>
 
 
@@ -18,21 +18,21 @@
  *************************************/
 void drawAxes (void)
 {
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_LINES);
-	  glVertex3f(0.0, 0.0, 0.0);
-	  glVertex3f(2.0, 0.0, 0.0);
-	  glVertex3f(0.0, 0.0, 0.0);
-	  glVertex3f(0.0, 2.0, 0.0);
-	  glVertex3f(0.0, 0.0, 0.0);
-	  glVertex3f(0.0, 0.0, 2.0);
-	glEnd();
-	glRasterPos3f(2.1, 0.1, 0.0);
-	glutBitmapCharacter(GLUT_BITMAP_8_BY_13, 'x');
-	glRasterPos3f(0.1, 2.1, 0.0);
-	glutBitmapCharacter(GLUT_BITMAP_8_BY_13, 'y');
-	glRasterPos3f(0.1, 0.1, 2.0);
-	glutBitmapCharacter(GLUT_BITMAP_8_BY_13, 'z');
+    glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(2.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 2.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 2.0);
+    glEnd();
+    glRasterPos3f(2.1, 0.1, 0.0);
+    glutBitmapCharacter(GLUT_BITMAP_8_BY_13, 'x');
+    glRasterPos3f(0.1, 2.1, 0.0);
+    glutBitmapCharacter(GLUT_BITMAP_8_BY_13, 'y');
+    glRasterPos3f(0.1, 0.1, 2.0);
+    glutBitmapCharacter(GLUT_BITMAP_8_BY_13, 'z');
 }
 
 /*************************************
@@ -40,33 +40,45 @@ void drawAxes (void)
  *************************************/
 void display (void)
 {
-	glClear (GL_COLOR_BUFFER_BIT);
-	//glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear (GL_COLOR_BUFFER_BIT);
+    //glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-	//isometric view in upper left corner
-	gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glViewport(20, 300, 250, 250);
-	drawAxes();
+    //isometric view in upper left corner
+//    glLoadIdentity();
+    glPushMatrix();
+    gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glViewport(20, 300, 250, 250);
+    drawAxes();
 
-	//**** Call to glut 3D functions go here
+    //**** Call to glut 3D functions go here
+    // radius, slices, stacks
+    // slices: # divs around z axis (latitudinal)
+    // stacks: # divs along z axis (longitudinal)
+    glutWireSphere(1.0, 10.0, 5.0);
+    glPopMatrix();
 
-	//side elevation in upper right corner
-	glLoadIdentity();
-	gluLookAt(10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glViewport(300, 300, 250, 250);
-	drawAxes();
+    //side elevation in upper right corner
+    glPushMatrix();
+    gluLookAt(10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glViewport(300, 300, 250, 250);
+    drawAxes();
 
-	//**** Call to glut 3D functions go here
+    //**** Call to glut 3D functions go here
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glutWireSphere(1.0, 10.0, 5.0);
+    glPopMatrix();
 
-	//front elevation in lower left corner
-	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glViewport(20, 20, 250, 250);
-	drawAxes();
+    //front elevation in lower left corner
+    glPushMatrix();
+    gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glViewport(20, 20, 250, 250);
+    drawAxes();
 
-	//**** Call to glut 3D functions go here
+    //**** Call to glut 3D functions go here
+    glutWireSphere(1.0, 10.0, 5.0);
+    glPopMatrix();
 
-	glFlush ();
+    glFlush ();
 }
 
 /*************************************
@@ -74,8 +86,9 @@ void display (void)
  *************************************/
 void init (void)
 {
-	glClearColor (1.0, 1.0, 1.0, 0.0);
-	//glEnable(GL_DEPTH_TEST);
+    glLoadIdentity();
+    glClearColor (1.0, 1.0, 1.0, 0.0);
+    //glEnable(GL_DEPTH_TEST);
 
 }
 
@@ -86,11 +99,11 @@ void init (void)
 
 void reshape (int w, int h)
 {
-	glMatrixMode (GL_PROJECTION);
-	glLoadIdentity ();
-	glOrtho (-3.0, 3.0, -3.0, 3.0, 5.0, 20.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    glOrtho (-3.0, 3.0, -3.0, 3.0, 5.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 /*************************************
@@ -98,15 +111,15 @@ void reshape (int w, int h)
  *************************************/
 int main (int argc, char** argv)
 {
-	glutInit (&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE|GLUT_RGB);
-	//glutInitDisplayMode (GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize (600, 600);
-	glutInitWindowPosition (100, 100);
-	glutCreateWindow (argv[0]);
-	glutReshapeFunc(reshape);
-	init ();
-	glutDisplayFunc (display);
-	glutMainLoop ();
-	return 0;
+    glutInit (&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE|GLUT_RGB);
+    //glutInitDisplayMode (GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
+    glutInitWindowSize (600, 600);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    glutReshapeFunc(reshape);
+    init ();
+    glutDisplayFunc (display);
+    glutMainLoop ();
+    return 0;
 }
---
+++ Labs/8-3dobjects/weiglej_3dobjects.c
@@ -0,0 +1,33 @@
+Q1:
+    Slices are divisions around the z axis; latitudinal divisions.
+
+Q2:
+    Stacks are divisions along the z axis: longitudinal divisions.
+
+Q3:
+    The sphere's center is on the origin, aka it extends an equal distance
+    (the radius) from the origin in every direction.
+
+Q4:
+    Doing either results in the sphere disappearing entirely.
+
+Q5:
+    It's hard to say with such a small sphere, but presumably, no.
+
+Q6:
+    The practical limit to the number of slices or stacks is one which the
+    sphere stops looking like a wire sphere and starts to look like a solid
+    sphere.
+
+Q7:
+    This limit is different for every sphere that has a different radius.
+    The limit would be higher the larger the radius a sphere has due to the
+    fact that larger radius spheres have a larger surface area and therefore
+    the distance between each 'wire' becomes greater. According to the formula
+    for the perimeter of a circle, C = 2PIr, C becomes greater as r increases.
+    When C becomes larger, dividing it by the same number produces larger 
+    results. The result of dividing C by a number gives you the distance
+    between each division in the wire sphere, whether it be a slice or a stack.
+
+Q8:
+
