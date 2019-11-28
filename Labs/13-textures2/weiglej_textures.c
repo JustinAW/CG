30c105dc0f38865cdd5d22276f86700b2370fc47
Justin Weigle <jw6858@cs.ship.edu>
2019-11-28 12:42:20 -0500

    q1-4 done

--- Labs/13-textures2/weiglej_textures.c
+++ Labs/13-textures2/weiglej_textures.c
@@ -6,3 +6,27 @@
  ************************************/
 
 Q1:
+    Setting both EYE_LINEAR and x + y + z = 0 results in the original vertical
+    magenta lines becoming curved. The curves are relative to the eye, or in
+    other terms, it is perpendicular to the base of the window regardless of
+    how the teapot oriented. This means that rotating the teapot, the curves
+    do not move.
+
+Q2:
+    x = 0 results in the original image when running the program where the
+    magenta lines on the teapot are all vertical no matter the orientation
+    of the teapot. The lines do not move when the teapot moves, just as in
+    q1.
+
+Q3:
+    This appears no different from q1 initially, but if the teapot is rotated,
+    the curved lines stay relative to the teapot, not the eye/window. They move
+    with the teapot.
+
+Q4:
+    This results in the lines staying perpendicular to the base of the teapot.
+    Just like q3, the lines move with the teapot.
+
+
+Q5:
+
